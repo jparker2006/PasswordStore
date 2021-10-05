603dc896fb5610f8eac931b9535dc432993440f8
jparker2006 <jakeparkeremail@gmail.com>
2021-10-05 04:08:18 -0700

    added real about page

--- JavaScript/MainMenu.js
+++ JavaScript/MainMenu.js
@@ -16,7 +16,7 @@ function MainMenuFrame() {
     sPage += "<div class='MainMenuBody'>";
 
     sPage += "<div class='MenuDivider FirstDivider'>"; // view passwords
-    sPage += "<br>View Stored Passwords";
+    sPage += "<br>View Passwords";
     sPage += "</div>";
 
     sPage += "<div class='MenuDivider SecondDivider'>"; // add password
@@ -42,10 +42,8 @@ function MainMenuFrame() {
 
 function HamburgerMenu() {
     let sPage = "";
-    sPage += "<div class='MenuHeaderImageContainer bouncy' title='More info on the project'>";
-    sPage += "<a href='https://github.com/jparker2006/PasswordStore/blob/master/README.md'>";
+    sPage += "<div class='MenuHeaderImageContainer bouncy' title='More info on the project' onClick='AboutProjectFrame()'>";
     sPage += "<img src='Images/InfoIcon.png' style='height: 52px; width: 52px;'>";
-    sPage += "</a>";
     sPage += "</div>";
 
     sPage += "<div class='MenuHeaderImageContainer bouncy' style='animation-delay:0.07s' title='feel free to look at and edit all the code behind this project'>";
@@ -78,4 +76,21 @@ function CloseHamburgerMenu() {
 function ToastIconClick() {
     document.getElementById('Toast').innerHTML = "<div class='ToastMsg'>Toast!</div>";
     setTimeout(function(){ document.getElementById('Toast').innerHTML = ''; }, 5000);
+}
+
+function AboutProjectFrame() {
+    let sPage = "";
+    sPage += "<div class='AboutFrame RoundedBox'>";
+    sPage += "Welcome to Password Store<br><br>";
+    sPage += "This site uses AES-256 encryption and SHA3-256 hashing to make sure your passwords kept safe and secure<br><br>";
+    sPage += "The best part is that I (the programmer) never get see your password or send it across the internet<br><br>";
+    sPage += "But dont trust me trust my code<br><br>";
+    sPage += "Password Store is completely open sourced on <a href='https://github.com/jparker2006/PasswordStore'>github</a><br><br>";
+    sPage += "If you have any questions you can contact me <a>here</a><br><br>"; // add when contact me is updated
+    sPage += "<div class='MenuDivider' onClick='MainMenuFrame()'>"; // view passwords
+    sPage += "<br>Back To Menu";
+    sPage += "</div>";
+    sPage += "</div>";
+
+    document.getElementById("Main").innerHTML = sPage;
 }
